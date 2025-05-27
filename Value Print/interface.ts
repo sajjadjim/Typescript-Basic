10f4a36c70a380c81eb83be5e36a3fcba2d610d5
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2025-05-27 22:44:36 +0600

    Create interface.ts
    Co-Authored-By: Md. Rifat Khan <120739312+rifatkhan109@users.noreply.github.com>

---
+++ Value Print/interface.ts
@@ -0,0 +1,11 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+const person: Person = {
+  name: "Sajjad Hossain JIM",
+  age: 25,
+};
+
+console.log(`${person.name} is ${person.age} years old.`);
