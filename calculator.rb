feb564783c10174e89b4d5c2b4b5cb5442982a6a
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-10-12 22:51:51 +0600

    Create calculator.rb
    Co-Authored-By: Fazle Rabbi Naim <mdfazlerabbi3278@gmail.com>

---
+++ calculator.rb
@@ -0,0 +1,35 @@
+# calculator.rb
+
+def calculator
+  puts "Welcome to Ruby Calculator!"
+  print "Enter first number: "
+  num1 = gets.chomp.to_f
+
+  print "Enter an operator (+, -, *, /): "
+  operator = gets.chomp
+
+  print "Enter second number: "
+  num2 = gets.chomp.to_f
+
+  result = case operator
+           when "+"
+             num1 + num2
+           when "-"
+             num1 - num2
+           when "*"
+             num1 * num2
+           when "/"
+             if num2 == 0
+               "Error: Division by zero!"
+             else
+               num1 / num2
+             end
+           else
+             "Invalid operator!"
+           end
+
+  puts "Result: #{result}"
+end
+
+# Run the calculator
+calculator
